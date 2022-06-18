3c33d9484264260316e723d2a7887f6c7aee6129
Tamim-uddin <muhammadtamimuddin@gmail.com>
2022-06-18 23:30:01 +0600

    firebase added

---
+++ src/Pages/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDzr806glrHATGaCGWh7u7CW9Mj5Kv2tng",
+    authDomain: "topten-29842.firebaseapp.com",
+    projectId: "topten-29842",
+    storageBucket: "topten-29842.appspot.com",
+    messagingSenderId: "668457696757",
+    appId: "1:668457696757:web:85ac5c64a511d9e94ef4bc",
+    measurementId: "G-LMK5DWQ7G1"
+  };
+
+  export default  firebaseConfig;
